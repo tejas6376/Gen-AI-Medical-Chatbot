44be1a42d02aeab62db86482e1bb790cda9d58c3
Tejas Mahajan <tejasmahajan101@gmail.com>
2025-01-10 00:35:21 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Tejas Mahajan',
+    author_email= 'tejasmahajan101@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
